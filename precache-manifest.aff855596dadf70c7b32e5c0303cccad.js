b51c0fa57cc9882d928ed0e94306ee7b7a1fbaf3
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-06-28 20:07:14 +0200

    Updates

---
+++ precache-manifest.aff855596dadf70c7b32e5c0303cccad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbe2e8c774cdc253a4a55cacb5c8cf9",
+    "url": "/my-reads/index.html"
+  },
+  {
+    "revision": "e05eedf8f67404f2bfc1",
+    "url": "/my-reads/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c64c1a2587ce01b30fd3",
+    "url": "/my-reads/static/css/main.8c4f1e1e.chunk.css"
+  },
+  {
+    "revision": "e05eedf8f67404f2bfc1",
+    "url": "/my-reads/static/js/2.9680d831.chunk.js"
+  },
+  {
+    "revision": "238881b61507ee3131d9dc2b2dc824c7",
+    "url": "/my-reads/static/js/2.9680d831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64c1a2587ce01b30fd3",
+    "url": "/my-reads/static/js/main.64d9e6e9.chunk.js"
+  },
+  {
+    "revision": "aadda5aa691f279f6ac9",
+    "url": "/my-reads/static/js/runtime-main.2e697786.js"
+  },
+  {
+    "revision": "bfc5d976f135767ad93939ff3693e161",
+    "url": "/my-reads/static/media/app-logo.bfc5d976.svg"
+  }
+]);
