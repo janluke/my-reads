98ecde0014374aa40e35cdc884a3d6b96e67dc2a
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-07-14 01:57:18 +0200

    Replace HashRouter with BrowserRouter
    HashRouter was used because it made easier to deploy on gh-pages.
    Since I'm going to deploy on Netlify, I can use BrowserRouter again.
    
    This also solves an issue with hash links for bookshelves, which
    don't work with HashRouter.

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import cn from 'classnames';
 import { SvgPageNotFound } from 'iblis-react-undraw';
 import { toast, ToastContainer } from 'react-toastify';
